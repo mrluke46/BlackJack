d8b4bddc881f487b26a597dfaa52cddc6e95d26d
Luke <81770748+mrluke46@users.noreply.github.com>
2022-01-18 16:16:56 +0700

    test

--- PYBJPP.py
+++ PYBJPP.py
@@ -1,1 +1,7 @@
-# This project is the personal project; Black Jack project first created Jan 18, 2022.
+# This project is the personal project; Black Jack project first created on Jan 18, 2022.
+
+def _APPINFO() :
+    class s_INFO:
+        developer = "Mr.Snowman"
+
+print("a")
