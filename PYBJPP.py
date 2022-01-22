59d0ee1732e82527af320b133841401cbd4f27ec
Luke <81770748+mrluke46@users.noreply.github.com>
2022-01-22 19:02:02 +0700

    v0.11

--- PYBJPP.py
+++ PYBJPP.py
@@ -1,7 +1,7 @@
 # This project is the personal project; Black Jack project first created on Jan 18, 2022.
 
-def _APPINFO() :
-    class s_INFO:
-        developer = "Mr.Snowman"
+def _APPINFO() : # Contains the infomation about this application.
+    _INFO = {"developer" : "Mr.Snowman", "version" : "0.11", "build" : "20220122", "aCode" : "PYBJPP"} # build format: YYYYMMDD
+    return _INFO
 
-print("RUN")
+from pybjpp_database import *
---
+++ pybjpp_database.py
@@ -0,0 +1,7 @@
+class Card: # value of the cards in the deck.
+    number_card = [2, 3, 4, 5, 6, 7, 8, 9, 10] # value of normal Number cards.
+    a_card = [1, 11] # value of Ace card (A).
+    f_card = [10] # value of every Face cards (K, Q, J).
+
+
+cards = {'Aa':1, 'Ab':11, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10,'Q':10,'K':10}
